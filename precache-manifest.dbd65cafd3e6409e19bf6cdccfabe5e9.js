b321ceb80ec72fd9dc561fb6c63755a052764dfa
Fanxing Xu <stella@turingvideo.com>
2019-07-21 17:24:04 -0700

    update

---
+++ precache-manifest.dbd65cafd3e6409e19bf6cdccfabe5e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694f1160bdd934aba4cff13d4bc528f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ab3b9c263cf53f9677",
+    "url": "/static/css/main.b95848af.chunk.css"
+  },
+  {
+    "revision": "bd5f0b5ac78ebccd378d",
+    "url": "/static/js/2.a89b4503.chunk.js"
+  },
+  {
+    "revision": "b1ab3b9c263cf53f9677",
+    "url": "/static/js/main.ff4814a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37f4cf575eeb96ee86373605dd1f8962",
+    "url": "/static/media/logo.37f4cf57.jpg"
+  }
+]);
