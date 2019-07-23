b104df717da6e9d942ba6b2d0708198b43d87e60
Fanxing Xu <stella@turingvideo.com>
2019-07-23 16:24:57 -0700

    Updates

---
+++ precache-manifest.c13318e7c2eba98eb7e7c89798ff62db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93653c4fe4ca9985b2877cbb69eca5a4",
+    "url": "/Turing-Gear/index.html"
+  },
+  {
+    "revision": "56b1393328154fec88f3",
+    "url": "/Turing-Gear/static/css/main.2b176d4c.chunk.css"
+  },
+  {
+    "revision": "bd5f0b5ac78ebccd378d",
+    "url": "/Turing-Gear/static/js/2.a89b4503.chunk.js"
+  },
+  {
+    "revision": "56b1393328154fec88f3",
+    "url": "/Turing-Gear/static/js/main.a95fdce7.chunk.js"
+  },
+  {
+    "revision": "49bd0ed0b009be980b8b",
+    "url": "/Turing-Gear/static/js/runtime~main.39c2786c.js"
+  },
+  {
+    "revision": "37f4cf575eeb96ee86373605dd1f8962",
+    "url": "/Turing-Gear/static/media/logo.37f4cf57.jpg"
+  }
+]);
